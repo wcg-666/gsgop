fc4e9812662c3b0d841252f4e55557dad254832d
wcg-666 <1215389641@qq.com>
2020-12-06 16:44:56 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+入口js
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
